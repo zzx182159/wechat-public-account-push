a58127541e3b0a4179bc9cf62e2f38af4ab9272c
zzx182159 <1821590358@qq.com>
2024-12-28 20:08:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oEPFF7A-1Frl7z9IpaxkXpvWbooM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	A_fjQx74rmkBeGJYxFhJLRtlMpeCcn8LFb7pg-RNh6c',
+      useTemplateId: '		fI8VUdesMgAH5j6uHahbQK4fNFuYkB6wCeZ57MIoIYU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
